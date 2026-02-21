29932fa16dfa13cf607b13eeb2ad5cfd719c2c9d
insaifali <insaifali090@gmail.com>
2026-02-21 15:30:23 +0530

    v1: initial Maven webapp scaffold

---
+++ src/main/java/com/oceanview/servlet/HelloServlet.java
@@ -0,0 +1,16 @@
+package com.oceanview.servlet;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h2>Hello from Ocean View Resort!</h2>");
+    }
+}
